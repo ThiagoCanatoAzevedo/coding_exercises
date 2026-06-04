8b48b6a79e14a4d2e3c8f3bf877543b4f8376149
ThiagoCanatoAzevedo <thi.canato@gmail.com>
2026-06-04 18:20:03 -0300

    change folders name

---
+++ Leetcode/2026/05_Maio/permutations.py
@@ -0,0 +1,24 @@
+def permute(nums):
+    all_permutations = []
+    
+    # for i, v in enumerate(nums):
+
+        
+
+
+
+
+"""
+Anotações importantes por exercício:
+- Nome exercício:
+- Data realizado:
+- Tempo de desenvolvimento:
+- Dificuldade (leetcode):
+- Dificuldade (pessoal):
+- Tópico trabalhado:
+- Complexidade (Time/space):
+
+- Resolvi sozinho? (sim / dica / solução):
+- Resumo da solução/ideia:
+- Onde travei:
+"""
