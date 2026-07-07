6048b9d5b01e74ecc4c4e3800b428fcbc7c4ac58
ThiagoCanatoAzevedo <thi.canato@gmail.com>
2026-07-07 20:18:15 -0300

    new exercise "lenght of last word"

--- Leetcode/2026/06_Junho/length_of_last_word.py
+++ Leetcode/2026/06_Junho/length_of_last_word.py
@@ -4,4 +4,17 @@ def lengthOfLastWord(s):
 
 print(lengthOfLastWord(s = "   fly me   to   the moon  "))
 
-e
+"""
+Anotações importantes por exercício:
+- Nome exercício: Length of Last Word
+- Data realizado: 13/06/2026
+- Tempo de desenvolvimento: 5 minutos
+- Dificuldade (leetcode): Easy
+- Dificuldade (pessoal): Easy
+- Tópico trabalhado: String
+- Complexidade (Time/space): O(n) e O(n)
+
+- Resolvi sozinho? (sim / dica / solução): Sim
+- Resumo da solução/ideia: Apenas dar um splitt na string através do espaço dela, gerando uma lista e, assim, pegando o tamanho do último valor da lista
+- Onde travei: Não travei
+"""
